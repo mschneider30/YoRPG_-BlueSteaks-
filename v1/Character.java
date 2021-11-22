eb6d77fa065bb0f27e25335a9bdf27efcc4428e4
mschneider30 <mschneider30@stuy.edu>
2021-11-22 13:16:07 -0500

    Added v1

---
+++ v1/Character.java
@@ -0,0 +1,11 @@
+public class Character {
+  public int health;
+  public int strength;
+  public int defense;
+  public double attack;
+
+  public boolean isAlive(){
+    return health > 0;
+  }
+
+}
